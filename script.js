8e581e9325ca89de3a23b5ec89ee86badbd3ffcc
gmLightt <abdelrhmn763@gmail.com>
2026-06-24 01:58:32 +0300

    update: added end value selection

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const generator = document.getElementById('generator');
 const displayNum = document.getElementById('display');
-let randomNum = ''
+const form = document.getElementById('myForm');
+const endNumInput = document.getElementById('endNum');
+const valueOutput = document.getElementById('values');
+let randomNum = '';
+let userData = '';
+
+form.addEventListener('submit', function(event) {
+  
+  event.preventDefault();
+  
+  userData = endNumInput.value;
+  valueOutput.textContent = `end value set to: ${userData}`;
+});
+
+
 
 generator.addEventListener('click', function() {
-  let randomNum = Math.floor(Math.random() * 100) + 1;
+  let randomNum = Math.floor(Math.random() * userData) + 1;
   displayNum.innerHTML = 'Number: ' + randomNum;
   console.log('success!')
 });
